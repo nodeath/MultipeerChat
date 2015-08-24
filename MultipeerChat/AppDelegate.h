132708ba70b4607564897dfad1a4b82d86d66e13
Shawn Hung <shawnhung@paganiniplus.com>
2015-08-24 11:39:53 +0800

    Initial Commit

---
+++ MultipeerChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultipeerChat
+//
+//  Created by Shawn Hung on 8/24/15.
+//  Copyright (c) 2015 Paganini Plus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultipeerChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultipeerChat
+//
+//  Created by Shawn Hung on 8/24/15.
+//  Copyright (c) 2015 Paganini Plus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
